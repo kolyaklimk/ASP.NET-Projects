2fde555cff785979b82cd67c9310bbe1a6238799
Kolya <kolyaklimk@gmail.com>
2023-09-05 10:01:33 +0300

    Create ASP.NET project

---
+++ 5th semester university/Lab 1/WEB_153504_Klimkovich/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WEB_153504_Klimkovich.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
