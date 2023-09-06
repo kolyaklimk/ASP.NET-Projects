cd143ee5bfa020be0264fed92e2c477bbf2d8837
Nikolay Klimkovich <kolyaklimk@gmail.com>
2023-09-06 16:39:55 +0300

    add tag-helpers and viewdata

--- 5th semester university/Lab 1-3/WEB_153504_Klimkovich/Controllers/HomeController.cs
+++ 5th semester university/Lab 1-3/WEB_153504_Klimkovich/Controllers/HomeController.cs
@@ -4,8 +4,12 @@ namespace WEB_153504_Klimkovich.Controllers
 {
     public class HomeController : Controller
     {
+        [ViewData]
+        public string NameLab { get; set; }
+
         public IActionResult Index()
         {
+            NameLab = "Лабораторная работа №2";
             return View();
         }
     }
