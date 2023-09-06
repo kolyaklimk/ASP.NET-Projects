c06eb02b7fa2d8da9f092d4dc52d83ba8036302c
Nikolay Klimkovich <kolyaklimk@gmail.com>
2023-09-06 15:42:45 +0300

    Add db to project

---
+++ 5th semester university/Lab 1/WEB_153504_Klimkovich/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WEB_153504_Klimkovich.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
