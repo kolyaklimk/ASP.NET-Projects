c5fa0188e377fa44ac42cf949ae01043b08cc330
Kolya <kolyaklimk@gmail.com>
2023-09-08 00:34:13 +0300

    update

---
+++ 5th semester university/Lab 1-3/WEB_153504_Klimkovich/Components/Cart.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_153504_Klimkovich.Components
+{
+    public class Cart : ViewComponent
+    {
+        private IRepository repository;
+        public Cart(IRepository repo)
+        {
+            repository = repo;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(repository.GetAll());
+        }
+
+    }
+}
--- 5th semester university/Lab 1-3/WEB_153504_Klimkovich/Controllers/HomeController.cs
+++ 5th semester university/Lab 1-3/WEB_153504_Klimkovich/Controllers/HomeController.cs
@@ -6,21 +6,23 @@ namespace WEB_153504_Klimkovich.Controllers
 {
     public class HomeController : Controller
     {
-        /*[ViewData]
+        [ViewData]
         public string NameLab { get; set; }
 
         [ViewData]
-        public List<ListDemo> ListDemo { get; set; }*/
+        public SelectList ListDemo { get; set; }
 
         public IActionResult Index()
         {
-            ViewData["NameLab"] = "Лабораторная работа №2";
-            ViewData["ListDemo"] = new SelectList(new List<ListDemo>
+            //ViewData["NameLab"] = "Лабораторная работа №2";
+
+            ListDemo = new SelectList(new List<ListDemo>
             {
                 new ListDemo { Id = 1, Name = "Элемент 1" },
                 new ListDemo { Id = 2, Name = "Элемент 2" },
                 new ListDemo { Id = 3, Name = "Элемент 3" }
             }, "Id", "Name");
+            NameLab = "Лабораторная работа №2";
 
             return View();
         }
