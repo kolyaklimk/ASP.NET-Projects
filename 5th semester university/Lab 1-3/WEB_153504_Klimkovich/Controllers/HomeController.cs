d95584bcd5585bd31e0b5169fcbb1af81f092b67
Nikolay Klimkovich <kolyaklimk@gmail.com>
2023-09-07 22:04:17 +0300

    Add SelectList

--- 5th semester university/Lab 1-3/WEB_153504_Klimkovich/Controllers/HomeController.cs
+++ 5th semester university/Lab 1-3/WEB_153504_Klimkovich/Controllers/HomeController.cs
@@ -1,15 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WEB_153504_Klimkovich.Entities;
 
 namespace WEB_153504_Klimkovich.Controllers
 {
     public class HomeController : Controller
     {
-        [ViewData]
+        /*[ViewData]
         public string NameLab { get; set; }
+
+        [ViewData]
+        public List<ListDemo> ListDemo { get; set; }*/
 
         public IActionResult Index()
         {
-            NameLab = "Лабораторная работа №2";
+            ViewData["NameLab"] = "Лабораторная работа №2";
+            ViewData["ListDemo"] = new SelectList(new List<ListDemo>
+            {
+                new ListDemo { Id = 1, Name = "Элемент 1" },
+                new ListDemo { Id = 2, Name = "Элемент 2" },
+                new ListDemo { Id = 3, Name = "Элемент 3" }
+            }, "Id", "Name");
+
             return View();
         }
     }
---
+++ 5th semester university/Lab 1-3/WEB_153504_Klimkovich/Entities/ListDemo.cs
@@ -0,0 +1,7 @@
+﻿namespace WEB_153504_Klimkovich.Entities;
+
+public class ListDemo
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
