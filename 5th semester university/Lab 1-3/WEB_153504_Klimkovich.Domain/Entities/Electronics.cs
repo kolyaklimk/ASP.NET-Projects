c9a52f9164621718dcd56b1bc62f353ac4e3abae
Nikolay Klimkovich <kolyaklimk@gmail.com>
2023-09-18 20:14:28 +0300

    Add Category

--- 5th semester university/Lab 1-3/WEB_153504_Klimkovich.Domain/Entities/Electronics.cs
+++ 5th semester university/Lab 1-3/WEB_153504_Klimkovich.Domain/Entities/Electronics.cs
@@ -6,6 +6,7 @@
         public string Name { get; set; } = null!;
         public string Description { get; set; } = null!;
         public int CategoryId { get; set; }
+        public Category Category { get; set; }
         public decimal Price { get; set; }
         public string Image { get; set; } = null!;
         public string Mime { get; set; } = null!;
