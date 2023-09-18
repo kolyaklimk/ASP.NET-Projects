f952f8baa5387dd2dfad9f1cbe3ad9f39475af87
Nikolay Klimkovich <kolyaklimk@gmail.com>
2023-09-18 20:14:52 +0300

    Create ICategoryService.cs

---
+++ 5th semester university/Lab 1-3/WEB_153504_Klimkovich/Services/CategoryService/ICategoryService.cs
@@ -0,0 +1,10 @@
+﻿using WEB_153504_Klimkovich.Domain.Entities;
+using WEB_153504_Klimkovich.Domain.Models;
+
+namespace WEB_153504_Klimkovich.Services.CategoryService
+{
+    public interface ICategoryService
+    {
+        public Task<ResponseData<List<Category>>> GetCategoryListAsync();
+    }
+}
