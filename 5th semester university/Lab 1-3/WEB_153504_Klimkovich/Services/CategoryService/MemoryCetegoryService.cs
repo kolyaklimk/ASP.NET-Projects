8d01dc2e84610018d21df35a19e5fc28304dbe74
Nikolay Klimkovich <kolyaklimk@gmail.com>
2023-09-18 20:14:59 +0300

    Create MemoryCetegoryService.cs

---
+++ 5th semester university/Lab 1-3/WEB_153504_Klimkovich/Services/CategoryService/MemoryCetegoryService.cs
@@ -0,0 +1,26 @@
+﻿using WEB_153504_Klimkovich.Domain.Entities;
+using WEB_153504_Klimkovich.Domain.Models;
+
+namespace WEB_153504_Klimkovich.Services.CategoryService
+{
+    public class MemoryCategoryService : ICategoryService
+    {
+        public Task<ResponseData<List<Category>>> GetCategoryListAsync()
+        {
+            var categories = new List<Category>
+            {
+                new Category {Id=1, Name="Смартфоны",
+                    NormalizedName="smartphones"},
+                 new Category {Id=2, Name="Наушники",
+                     NormalizedName="headphones"},
+                 new Category {Id=3, Name="Клавиатуры",
+                     NormalizedName="keyboards"},
+            };
+
+            var result = new ResponseData<List<Category>>();
+            result.Data = categories;
+            return Task.FromResult(result);
+        }
+    }
+
+}
