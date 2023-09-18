3508eb8626340972154a744873cba94b77ce3733
Nikolay Klimkovich <kolyaklimk@gmail.com>
2023-09-18 20:14:34 +0300

    Create ProductListModel.cs

---
+++ 5th semester university/Lab 1-3/WEB_153504_Klimkovich.Domain/Models/ProductListModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WEB_153504_Klimkovich.Domain.Models
+{
+    public class ListModel<T>
+    {
+        public List<T> Items { get; set; } = new();
+        public int CurrentPage { get; set; } = 1;
+        public int TotalPages { get; set; } = 1;
+    }
+}
