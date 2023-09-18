32bc334e1269eb86a5264b8413f18b425e51460c
Nikolay Klimkovich <kolyaklimk@gmail.com>
2023-09-18 20:14:37 +0300

    Create ResponseData.cs

---
+++ 5th semester university/Lab 1-3/WEB_153504_Klimkovich.Domain/Models/ResponseData.cs
@@ -0,0 +1,9 @@
+﻿namespace WEB_153504_Klimkovich.Domain.Models
+{
+    public class ResponseData<T>
+    {
+        public T Data { get; set; }
+        public bool Success { get; set; } = true;
+        public string? ErrorMessage { get; set; }
+    }
+}
