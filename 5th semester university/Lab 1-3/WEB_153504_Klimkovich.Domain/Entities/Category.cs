de3d94a5c81c7ba598b2b19e51b149ea8867bb56
Nikolay Klimkovich <kolyaklimk@gmail.com>
2023-09-18 16:49:00 +0300

    Add Entities

---
+++ 5th semester university/Lab 1-3/WEB_153504_Klimkovich.Domain/Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace WEB_153504_Klimkovich.Domain.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string NormalizedName { get; set; } = null!;
+    }
+}
---
+++ 5th semester university/Lab 1-3/WEB_153504_Klimkovich.Domain/Entities/Electronics.cs
@@ -0,0 +1,14 @@
+﻿namespace WEB_153504_Klimkovich.Domain.Entities
+{
+    public class Electronics
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public int CategoryId { get; set; }
+        public decimal Price { get; set; }
+        public string Image { get; set; } = null!;
+        public string Mime { get; set; } = null!;
+    }
+
+}
