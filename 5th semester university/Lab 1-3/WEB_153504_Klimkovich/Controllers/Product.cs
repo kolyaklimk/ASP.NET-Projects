13206774f300d4a58695749c22f18c2acc8f780d
Nikolay Klimkovich <kolyaklimk@gmail.com>
2023-09-18 20:15:07 +0300

    Create Product.cs

---
+++ 5th semester university/Lab 1-3/WEB_153504_Klimkovich/Controllers/Product.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WEB_153504_Klimkovich.Domain.Entities;
+using WEB_153504_Klimkovich.Services.CategoryService;
+using WEB_153504_Klimkovich.Services.ProductService;
+
+namespace WEB_153504_Klimkovich.Controllers
+{
+    public class Product : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly ICategoryService _categoryService;
+
+        public Product(IProductService product, ICategoryService category)
+        {
+            _categoryService = category;
+            _productService = product;
+        }
+
+        public async Task<IActionResult> Index(string? category)
+        {
+            var productResponse = await _productService.GetProductListAsync(category);
+
+            if (!productResponse.Success)
+                return NotFound(productResponse.ErrorMessage);
+
+            return View(productResponse.Data.Items);
+        }
+    }
+}
