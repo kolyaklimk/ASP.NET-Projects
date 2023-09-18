17fec421f434993f6bfc5446adb57badb72685c1
Nikolay Klimkovich <kolyaklimk@gmail.com>
2023-09-18 20:14:55 +0300

    Create IProductService.cs

---
+++ 5th semester university/Lab 1-3/WEB_153504_Klimkovich/Services/ProductService/IProductService.cs
@@ -0,0 +1,21 @@
+﻿using WEB_153504_Klimkovich.Domain.Entities;
+using WEB_153504_Klimkovich.Domain.Models;
+
+namespace WEB_153504_Klimkovich.Services.ProductService
+{
+    public interface IProductService
+    {
+
+        public Task<ResponseData<ListModel<Electronics>>> GetProductListAsync(string?
+       categoryNormalizedName, int pageNo = 1);
+
+        public Task<ResponseData<Electronics>> GetProductByIdAsync(int id);
+
+        public Task UpdateProductAsync(int id, Electronics product, IFormFile? formFile);
+
+        public Task DeleteProductAsync(int id);
+
+        public Task<ResponseData<Electronics>> CreateProductAsync(Electronics product, IFormFile?
+       formFile);
+    }
+}
