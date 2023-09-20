83fca0bbfa903bad109f4dfcc5ea965a399dc4d0
Kolya <kolyaklimk@gmail.com>
2023-09-20 12:01:19 +0300

    Add SeedData

---
+++ 5th semester university/Labs/WEB_153504_Klimkovich.API/Data/DbInitializer.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WEB_153504_Klimkovich.Domain.Entities;
+
+namespace WEB_153504_Klimkovich.API.Data
+{
+    public class DbInitializer
+    {
+        public static async Task SeedData(WebApplication app)
+        {
+            using (var scope = app.Services.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                await context.Database.MigrateAsync();
+
+                var imageUrl = app.Configuration["ImageUrl"];
+
+                var categories = new List<Category>
+                {
+                    new Category { Name="Смартфоны",
+                        NormalizedName="smartphones"},
+                    new Category { Name="Наушники",
+                        NormalizedName="headphones"},
+                    new Category { Name="Клавиатуры",
+                        NormalizedName="keyboards"},
+                    new Category {Name="USB",
+                        NormalizedName="usb"},
+                };
+
+                var electronics = new List<Electronics>
+                {
+                    new Electronics { Name="Redmi Note 5",
+                        Description="Snapdragon 636",
+                        Price = 199, Image=imageUrl+"/images/redmiNote5.jpg",
+                        Category= categories.Find(c=>c.NormalizedName.Equals("smartphones"))},
+
+                    new Electronics { Name="Huawei P Smart 2019",
+                        Description="Kirin 710",
+                        Price = 149, Image=imageUrl+"/images/huaweiPSmart.jpg",
+                        Category= categories.Find(c=>c.NormalizedName.Equals("smartphones"))},
+
+                    new Electronics { Name="Huawei 2020",
+                        Description="Kirin 810",
+                        Price = 230, Image=imageUrl+ "/images/huawei2020.jpg",
+                        Category= categories.Find(c=>c.NormalizedName.Equals("smartphones"))},
+
+                    new Electronics { Name="Huawei 2017",
+                        Description="Kirin 510",
+                        Price = 82, Image=imageUrl+"/images/huawei2017.jpg",
+                        Category= categories.Find(c=>c.NormalizedName.Equals("smartphones"))},
+
+                    new Electronics { Name="Redmi Note 8 Pro",
+                        Description="Snapdragon 712",
+                        Price = 222, Image=imageUrl+"/images/redmiNote8pro.jpg",
+                        Category= categories.Find(c=>c.NormalizedName.Equals("smartphones"))},
+
+                    new Electronics { Name="Beats",
+                        Description="White, big",
+                        Price = 123, Image=imageUrl+"/images/beats.jpg",
+                        Category= categories.Find(c=>c.NormalizedName.Equals("headphones"))},
+
+                    new Electronics { Name="Sony xk840",
+                        Description="Blue",
+                        Price = 235, Image=imageUrl+"/images/sonyxk840.jpg",
+                        Category= categories.Find(c=>c.NormalizedName.Equals("headphones"))},
+
+                    new Electronics { Name="AquaKey",
+                        Description="Red",
+                        Price = 53, Image=imageUrl+"/images/aquakey.jpg",
+                        Category= categories.Find(c=>c.NormalizedName.Equals("keyboards"))},
+
+                    new Electronics { Name="RGB Serial 2820",
+                        Description="RGB",
+                        Price = 75, Image=imageUrl+"/images/rgbserial2820.jpg",
+                        Category= categories.Find(c=>c.NormalizedName.Equals("keyboards"))},
+
+                    new Electronics { Name="GrassKey",
+                        Description="Green",
+                        Price = 111, Image=imageUrl+"/images/grasskey.jpg",
+                        Category= categories.Find(c=>c.NormalizedName.Equals("keyboards"))},
+                };
+
+                context.Categories.AddRange(categories);
+                context.Electronics.AddRange(electronics);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
