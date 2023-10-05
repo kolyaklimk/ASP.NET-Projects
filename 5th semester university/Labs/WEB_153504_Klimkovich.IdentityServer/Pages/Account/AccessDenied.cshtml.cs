e37dc45f892e2b3bd2fe24a7f88b292015c03b44
Nikolay Klimkovich <kolyaklimk@gmail.com>
2023-10-05 21:49:12 +0300

    Add identityServer

---
+++ 5th semester university/Labs/WEB_153504_Klimkovich.IdentityServer/Pages/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WEB_153504_Klimkovich.IdentityServer.Pages.Account
+{
+    public class AccessDeniedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ 5th semester university/Labs/WEB_153504_Klimkovich.IdentityServer/Pages/Account/Logout/LogoutOptions.cs
@@ -0,0 +1,9 @@
+
+namespace WEB_153504_Klimkovich.IdentityServer.Pages.Logout
+{
+    public class LogoutOptions
+    {
+        public static bool ShowLogoutPrompt = true;
+        public static bool AutomaticRedirectAfterSignOut = false;
+    }
+}
